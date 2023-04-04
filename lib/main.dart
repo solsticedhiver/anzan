db20fb8f9cb5d185ad7a4aecf8b64d0a6be8f92e
solsTiCe d'Hiver <solstice.dhiver@gmail.com>
2023-04-04 17:12:58 +0200

    Add a settings route
    * rework main ui layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+import 'settings.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,16 +35,67 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _number = 0;
+  int _indx = 0;
+  Timer? _timer;
+  bool isReplayable = false;
+  bool isPlaying = false;
+  bool isVisible = false;
+  List<int> numbers = [];
 
   @override
   void initState() {
-    _number = Random().nextInt(99);
     super.initState();
   }
 
-  void _newRandomNumber() {
+  void _generateNumbers(int length, int digits) {
+    int startInt = pow(10, digits - 1).toInt();
+    int maxInt = pow(10, digits).toInt() - startInt;
+    debugPrint('maxInt=$maxInt, startInt=$startInt');
+    numbers =
+        List.generate(length, (index) => Random().nextInt(maxInt) + startInt);
+    debugPrint(numbers.toString());
+  }
+
+  void _nextRandomNumber() {
     setState(() {
-      _number = Random().nextInt(99);
+      isVisible = true;
+      _number = numbers[_indx];
+      _indx++;
+    });
+    Future.delayed(const Duration(milliseconds: 500), () {
+      setState(() {
+        isVisible = false;
+        if (_indx >= numbers.length) {
+          isPlaying = false;
+          _timer!.cancel();
+        }
+      });
+    });
+  }
+
+  void _replay() {
+    _indx = 0;
+    _timer = Timer.periodic(const Duration(milliseconds: 1000), (timer) {
+      if (!isPlaying) {
+        timer.cancel();
+      } else {
+        _nextRandomNumber();
+      }
+    });
+  }
+
+  void _startPlay() {
+    _indx = 0;
+    _generateNumbers(5, 2);
+    _timer = Timer.periodic(const Duration(milliseconds: 1000), (timer) {
+      if (!isPlaying) {
+        timer.cancel();
+      } else {
+        _nextRandomNumber();
+      }
+    });
+    setState(() {
+      isReplayable = true;
     });
   }
 
@@ -72,40 +127,132 @@ class _MyHomePageState extends State<MyHomePage> {
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Image.asset(
-                    'assets/soroban_logo_rounded.png',
-                    height: 100,
-                    width: 100,
-                  ),
+                  Row(children: [
+                    Image.asset(
+                      'assets/soroban_logo_rounded.png',
+                      height: 64,
+                      width: 64,
+                    ),
+                    const SizedBox(width: 15),
+                    Text('Flash Anzan',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: Theme.of(context)
+                                .textTheme
+                                .headlineMedium!
+                                .fontSize)),
+                  ]),
                   const SizedBox(
                     height: 15,
                   ),
                   const Expanded(
-                      flex: 1, child: Text('https://www.sorobanexam.org')),
+                      flex: 1,
+                      child: Text(
+                        'https://www.sorobanexam.org',
+                        style: TextStyle(color: Colors.white),
+                      )),
                 ]),
           ),
           Expanded(
               flex: 1,
-              child: ListView(children: const [
-                ListTile(title: Text('Settings')),
+              child: ListView(children: [
+                ListTile(
+                  leading: const Icon(Icons.settings),
+                  title: const Text('Settings'),
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => const SettingsRoute(),
+                      ),
+                    );
+                  },
+                ),
               ])),
         ],
       )),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Stack(
           children: <Widget>[
-            Text(
-              '$_number',
-              style: style,
-            ),
+            Visibility(
+                replacement: const SizedBox(height: 300),
+                visible: isVisible,
+                child: Text(
+                  '$_number',
+                  style: style,
+                )),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _newRandomNumber,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      bottomSheet: BottomSheet(
+        backgroundColor: Colors.brown[100],
+        builder: (context) {
+          return Container(
+              margin: const EdgeInsets.all(5),
+              child: Row(children: [
+                const Expanded(
+                    child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Your answer',
+                  ),
+                )),
+                const SizedBox(width: 8),
+                ClipOval(
+                    child: Container(
+                        width: 56,
+                        height: 56,
+                        color: const Color.fromARGB(255, 22, 131, 98),
+                        child: IconButton(
+                          color: Colors.white,
+                          icon: const Icon(Icons.check),
+                          onPressed: () {},
+                        ))),
+                const SizedBox(width: 8),
+                ClipOval(
+                    child: Container(
+                        width: 56,
+                        height: 56,
+                        color: const Color.fromARGB(255, 22, 131, 98),
+                        child: IconButton(
+                          color: Colors.white,
+                          icon: const Icon(Icons.replay),
+                          onPressed: isReplayable
+                              ? () {
+                                  setState(() {
+                                    isPlaying = true;
+                                  });
+                                  _replay();
+                                }
+                              : null,
+                        ))),
+                const SizedBox(width: 8),
+                ClipOval(
+                    child: Container(
+                        width: 56,
+                        height: 56,
+                        color: const Color.fromARGB(255, 22, 131, 98),
+                        child: IconButton(
+                          color: Colors.white,
+                          icon: isPlaying
+                              ? const Icon(Icons.stop)
+                              : const Icon(Icons.play_arrow),
+                          onPressed: () {
+                            setState(() {
+                              isPlaying = !isPlaying;
+                            });
+                            if (!isPlaying && _timer != null) {
+                              _timer!.cancel();
+                              isVisible = false;
+                            }
+                            if (isPlaying) {
+                              _startPlay();
+                            }
+                          },
+                        ))),
+              ]));
+        },
+        onClosing: () {},
       ),
     );
   }
