1dc40a03b7245701a4a2ca47502e2b0fc1e3171d
solsTiCe d'Hiver <solstice.dhiver@gmail.com>
2025-05-21 13:48:49 +0200

    Detect virt env when making flatpak

--- flatpak/make-flatpak.sh
+++ flatpak/make-flatpak.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
+#set -e
+
 buildtype=${1:-release}
-
-set -e
-
 CWD=`pwd`
 pushd ..
 flutter build linux --${buildtype}
@@ -13,13 +12,18 @@ ln -s ../build/linux/x64/${buildtype}/bundle anzan
 
 # build flatpak
 echo ':: running flatpak-builder'
-flatpak-builder --ccache --force-clean build-dir org.sorobanexam.anzan.yml
+if systemd-detect-virt --quiet; then
+	opts="--disable-cache --disable-rofiles-fuse"
+else
+	opts="--ccache"
+fi
+flatpak-builder "$opts" --force-clean build-dir org.sorobanexam.anzan.yml
 
-version=`grep ^version ../pubspec.yaml |cut -f 2 -d ' '|cut -f 1 -d '+'`
+#version=`grep ^version ../pubspec.yaml |cut -f 2 -d ' '|cut -f 1 -d '+'`
 sha=`git rev-parse --short main`
 rm -f anzan-*.flatpak
 # build flatpak single-file bundle
-echo ":: making flatpak single-file bundle: anzan-${version}-${sha}-x86_64.flatpak"
+echo ":: making flatpak single-file bundle: anzan-${sha}-x86_64.flatpak"
 flatpak build-export repo.d build-dir
 flatpak build-bundle repo.d anzan-${sha}-x86_64.flatpak org.sorobanexam.anzan
 
