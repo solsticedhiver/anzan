a0c1ece1d39b693d5c1e27a5c09bf3a28d7acd16
solsTiCe d'Hiver <solstice.dhiver@gmail.com>
2025-06-19 18:04:38 +0200

    Don't create source map on build release web

--- build-web.sh
+++ build-web.sh
@@ -2,7 +2,7 @@
 commit=`git rev-parse --short HEAD`
 # update commit version to latest HEAD
 sed -i "/.*commit/s/HEAD/$commit/" lib/config.dart
-flutter build web --base-href "/anzan.app/"
+flutter build web --base-href "/anzan.app/" --no-source-maps
 # also add a version to js to force reload, just in case
 sed -i "/flutter_bootstrap.js/s/js/js?$commit/" build/web/index.html
 rsync -av --delete ./build/web/ ~/prog/python/soroban/soroban-exam/soroxam/static_root/anzan.app/
