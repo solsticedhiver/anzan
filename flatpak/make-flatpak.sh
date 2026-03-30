2fe4c9b8e7f266a3ce6ae77bce61a2bbadde27d0
solsTiCe d'Hiver <solstice.dhiver@gmail.com>
2026-03-30 16:30:31 +0200

    Update script to build flatpak from local repo

--- flatpak/make-flatpak.sh
+++ flatpak/make-flatpak.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 #set -e
 
+#version=`grep ^version ../pubspec.yaml |cut -f 2 -d ' '|cut -f 1 -d '+'`
+sha=`git rev-parse --short main`
+long_sha=`git rev-parse main`
+
+# modify manifest to build local repo
+sed -i '/url:/s|https.*$|file:///home/solstice/prog/dvcs/anzan|' org.sorobanexam.anzan.yml
+sed -i "/commit:/s|: .*$|: $long_sha|" org.sorobanexam.anzan.yml
+
 # build flatpak
 echo ':: running flatpak-builder'
 if systemd-detect-virt --quiet; then
@@ -10,8 +18,6 @@ else
 fi
 flatpak-builder "$opts" --force-clean build-dir org.sorobanexam.anzan.yml
 
-#version=`grep ^version ../pubspec.yaml |cut -f 2 -d ' '|cut -f 1 -d '+'`
-sha=`git rev-parse --short main`
 rm -f anzan-*.flatpak
 # check for llvm20 extension
 if ! flatpak list |grep -q org.freedesktop.Sdk.Extension.llvm20 ;then
@@ -23,6 +29,7 @@ flatpak build-init build-dir org.sorobanexam.anzan org.freedesktop.Sdk org.freed
 flatpak build-export repo.d build-dir
 flatpak build-bundle repo.d anzan-${sha}-x86_64.flatpak org.sorobanexam.anzan
 
+# restore manifest
+git restore org.sorobanexam.anzan.yml
 rm -rf repo.d
 rm -rf build-dir
-rm -f anzan
