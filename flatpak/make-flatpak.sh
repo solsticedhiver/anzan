02b0f1f2a1bb38777a800edcf5970f8956118dd8
solsTiCe d'Hiver <solstice.dhiver@gmail.com>
2026-03-30 12:35:43 +0200

    Improve flatpak build files

--- flatpak/make-flatpak.sh
+++ flatpak/make-flatpak.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 #set -e
 
-buildtype=${1:-release}
-CWD=`pwd`
-pushd ..
-flutter build linux --${buildtype}
-popd
-
-rm -f anzan
-ln -s ../build/linux/x64/${buildtype}/bundle anzan
-
 # build flatpak
 echo ':: running flatpak-builder'
 if systemd-detect-virt --quiet; then
@@ -22,8 +13,13 @@ flatpak-builder "$opts" --force-clean build-dir org.sorobanexam.anzan.yml
 #version=`grep ^version ../pubspec.yaml |cut -f 2 -d ' '|cut -f 1 -d '+'`
 sha=`git rev-parse --short main`
 rm -f anzan-*.flatpak
+# check for llvm20 extension
+if ! flatpak list |grep -q org.freedesktop.Sdk.Extension.llvm20 ;then
+	flatpak install --user org.freedesktop.Sdk.Extension.llvm20
+fi
 # build flatpak single-file bundle
 echo ":: making flatpak single-file bundle: anzan-${sha}-x86_64.flatpak"
+flatpak build-init build-dir org.sorobanexam.anzan org.freedesktop.Sdk org.freedesktop.Platform
 flatpak build-export repo.d build-dir
 flatpak build-bundle repo.d anzan-${sha}-x86_64.flatpak org.sorobanexam.anzan
 
